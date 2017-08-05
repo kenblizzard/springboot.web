119793e1c1718b8e9756e11f277ddc731556f43c
kenblizzard <kenneth.bolico@gmail.com>
2017-08-05 23:17:50 +0800

    Initial version

---
+++ src/main/java/io/github/kenblizzard/imged/ImgedApplication.java
@@ -0,0 +1,12 @@
+package io.github.kenblizzard.imged;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImgedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImgedApplication.class, args);
+	}
+}
